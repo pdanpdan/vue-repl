5f46fcba071053bfd4f20491d97a3398b2924624
Dan Popescu <pdan.popescu@gmail.com>
2023-12-04 23:31:56 +0200

    feat(CodeMirror): load highlighter for sass and md

--- src/codemirror/codemirror.ts
+++ src/codemirror/codemirror.ts
@@ -7,6 +7,8 @@ import 'codemirror/mode/javascript/javascript.js'
 import 'codemirror/mode/css/css.js'
 import 'codemirror/mode/htmlmixed/htmlmixed.js'
 import 'codemirror/mode/vue/vue.js'
+import 'codemirror/mode/sass/sass.js'
+import 'codemirror/mode/gfm/gfm.js'
 
 // addons
 import 'codemirror/addon/edit/closebrackets.js'
